169ab05e50bc86c000538aff4e89c3f35257d41e
mingxuechao <mingxuechao@126.com>
2023-02-09 22:16:15 +0800

    增加hello world，

---
+++ featuredemos/helloword/popup.js
@@ -0,0 +1,9 @@
+//console.log("This is a popup!")
+
+var buttonStart = document.querySelector("#button_start");
+
+if (buttonStart) {
+    buttonStart.addEventListener("click", async () => {
+        console.log("start hello world");
+    });
+}
